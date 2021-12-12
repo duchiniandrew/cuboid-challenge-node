8f5dc9825e4b024c704b7c61ef78fcfb43d2d3a0
andrew.duchini <andrew.duchini@rarolabs.com.br>
2021-12-12 14:04:32 -0300

    feature: added trigger beforeInsert for table Cuboid

--- src/models/Cuboid.ts
+++ src/models/Cuboid.ts
@@ -11,6 +11,22 @@ export class Cuboid extends Base {
   bag!: Bag;
   volume!: number;
 
+  async $beforeInsert(): Promise<void> {
+    this.volume = this.width * this.height * this.depth;
+    await Bag.transaction(async (trx) => {
+      if (this.bagId) {
+        const bag = await Bag.query(trx).findById(this.bagId);
+        if (bag && bag.availableVolume >= this.volume) {
+          bag.availableVolume -= this.volume;
+          bag.payloadVolume += this.volume;
+          await Bag.query(trx).update(bag).where({ id: this.bagId });
+        } else {
+          throw new Error('Insufficient capacity in bag');
+        }
+      }
+    });
+  }
+
   static tableName = 'cuboids';
 
   static get relationMappings(): RelationMappings {
