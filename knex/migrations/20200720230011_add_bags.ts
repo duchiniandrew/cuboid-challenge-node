f06a1be1f43b60669b21bc730201382ae7a039d9
andrew.duchini <andrew.duchini@rarolabs.com.br>
2021-12-12 14:00:34 -0300

    fix: added missing column on table bags

--- knex/migrations/20200720230011_add_bags.ts
+++ knex/migrations/20200720230011_add_bags.ts
@@ -4,8 +4,12 @@ import { Bag } from '../../src/models';
 export const up = (knex: Knex): Promise<void> =>
   knex.schema.createTable(Bag.tableName, (table: Knex.TableBuilder) => {
     table.increments();
-    table.timestamps();
-    table.integer('volume');
+    table.string('title');
+    table.integer('volume').defaultTo(0);
+    table.integer('payloadVolume').defaultTo(0);
+    table.integer('availableVolume').defaultTo(0);
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
   });
 
 export const down = (knex: Knex): Promise<void> =>
