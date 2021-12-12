2dc645b71827100eab7fa522c4c82e34b5566a83
andrew.duchini <andrew.duchini@rarolabs.com.br>
2021-12-12 14:03:55 -0300

    fix: fixed problem table not joining

--- src/models/Cuboid.ts
+++ src/models/Cuboid.ts
@@ -17,7 +17,7 @@ export class Cuboid extends Base {
     return {
       bag: {
         relation: Base.BelongsToOneRelation,
-        modelClass: 'Bag',
+        modelClass: Bag,
         join: {
           from: 'cuboids.bagId',
           to: 'bags.id',
