602d53c329ddf5c14547f1bd03bb8265c4671c65
andrew.duchini <andrew.duchini@rarolabs.com.br>
2021-12-12 13:59:46 -0300

    fix: added missing column of table cuboids

--- knex/migrations/20200720201029_add_cuboids.ts
+++ knex/migrations/20200720201029_add_cuboids.ts
@@ -4,12 +4,14 @@ import { Knex } from 'knex';
 export const up = (knex: Knex): Promise<void> =>
   knex.schema.createTable(Cuboid.tableName, (table: Knex.TableBuilder) => {
     table.increments();
-    table.timestamps();
-    table.integer('width');
-    table.integer('height');
-    table.integer('depth');
+    table.integer('width').notNullable().defaultTo(0);
+    table.integer('height').notNullable().defaultTo(0);
+    table.integer('depth').notNullable().defaultTo(0);
+    table.integer('volume').notNullable().defaultTo(0);
     table.integer('bagId');
     table.foreign('bagId').references('id').inTable(Bag.tableName);
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
   });
 
 export const down = (knex: Knex): Promise<void> =>
